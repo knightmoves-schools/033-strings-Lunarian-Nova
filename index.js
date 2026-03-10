459d736f94f334a146f0d2bb19fd96eb213da366
Lunarian-Nova <ckeister@anamosa.k12.ia.us>
2026-03-10 17:08:17 +0000

    lkjhgfd

---
+++ index.js
@@ -0,0 +1,7 @@
+var firstName = "Lunarian";
+var jobTitle = "Software Developer";
+var city = "Nova City";
+
+var newJobPost = `${firstName} is looking for a ${jobTitle} job in ${city}`;
+
+document.getElementById('result').innerHTML = newJobPost;
